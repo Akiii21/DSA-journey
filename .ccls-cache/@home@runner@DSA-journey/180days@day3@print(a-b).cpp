99a8f29fdb9117c418e6c79fdee8c78d3a93d1e2
Akiii21 <121781517+Akiii21@users.noreply.github.com>
2024-01-25 19:52:02 +0000

    modified

--- .ccls-cache/@home@runner@DSA-journey/180days@day3@print(a-b).cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-  int a=4, b=3;
-  cout<<a-b<<endl;
-  return 0;
-}
---
+++ 180days/day4/sumof2number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int a,b;
+  cout<<"Enter the value of a"<<endl;
+  cin>>a;
+  cout<<"Enter the value of b"<<endl;
+  cin>>b;
+  cout<<"the sum of a and b is "<<a+b<<endl;
+  return 0;
+}
